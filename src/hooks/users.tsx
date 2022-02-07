1e06c22386c32f1c87e73a130e010db5ab47a4bf
Ariel Plascencia <imariel2d@gmail.com>
2022-02-07 10:30:54 -0700

    adding constants

--- src/hooks/users.tsx
+++ src/hooks/users.tsx
@@ -3,9 +3,11 @@ import { useQuery } from 'react-query';
 import { User } from '../types/user';
 import { QuerieKeys } from '../types/querie-keys';
 
+import { API_USER_ROUTE } from '../constants';
+
 export const useUsers = () => {
   const responses = useQuery<User[]>(QuerieKeys.Users, () =>
-    fetch('/users')
+    fetch(API_USER_ROUTE)
       .then((res) => res.json()),
   );
 
--- src/service/user.ts
+++ src/service/user.ts
@@ -1,9 +1,10 @@
 import axios from 'axios';
 
 import { User } from '../types/user';
+import { API_USER_ROUTE } from '../constants';
 
 export const addUser = (user: User): Promise<User> => {
-  return axios.post('/user', {
+  return axios.post(API_USER_ROUTE, {
     ...user
   });
 };
